f22a8e75ecf935c11119359278f3454a46f76f76
Petr <petr.ivanovich.1980@list.ru>
2019-05-21 20:53:06 +0300

    save in file with json

--- diplom project/DB/Saver.cs
+++ diplom project/DB/Saver.cs
@@ -4,44 +4,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
 
 namespace diplom_project.DB
-{
+{    
     class Saver
     {
-        BinaryWriter bw;
-        BinaryReader br;
+        //BinaryWriter bw;
+        //BinaryReader br;
 
-        public void Save (List<Model> files)
-        {
-            try
-            {
-                bw = new BinaryWriter(new FileStream(@"C:\Temp2\mydata.txt", FileMode.Append));
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine(e.Message + "\n Cannot create file.");
-                return;
-            }
-            try
-            {
-                bw.Write(DateTime.Now.ToString());
+        //public void Save(List<Model> files)
+        //{
+        //    try
+        //    {
+        //        bw = new BinaryWriter(new FileStream(@"C:\Temp2\mydata.txt", FileMode.Append));
+        //    }
+        //    catch (IOException e)
+        //    {
+        //        Console.WriteLine(e.Message + "\n Cannot create file.");
+        //        return;
+        //    }
+        //    try
+        //    {
+        //        bw.Write(DateTime.Now.ToString());
 
-                foreach(var file in files)
-                {
-                    bw.Write(file.Id);
-                    //bw.Write(file.Filename);
-                    //bw.Write(file.Hash);
-                    bw.Write(file.ShortName);
-                }
+        //        foreach (var file in files)
+        //        {
+        //            bw.Write(file.Id);
+        //            //bw.Write(file.Filename);
+        //            //bw.Write(file.Hash);
+        //            bw.Write(file.ShortName);
+        //        }
+        //    }
+        //    catch (IOException e)
+        //    {
+        //        Console.WriteLine(e.Message + "\n Cannot write to file.");
+        //        return;
+        //    }
+        //    bw.Close();
+
+        //}
+        DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(List<Model>));
+
+        public void Save(List<Model> files)
+        {            
+
+            using (FileStream fs = new FileStream(@"C:\Temp2\mydata.json", FileMode.Append))
+            {                
+                jsonFormatter.WriteObject(fs, files);
             }
-            catch (IOException e)
-            {
-                Console.WriteLine(e.Message + "\n Cannot write to file.");
-                return;
-            }
-            bw.Close();
 
         }
+
+        public List<Model> Write()
+        {
+            List < Model > rew = new List<Model>();
+            using (FileStream fs = new FileStream(@"C:\Temp2\mydata.json", FileMode.OpenOrCreate))
+            {
+                rew = (List<Model>)jsonFormatter.ReadObject(fs);
+                
+            }
+
+            return rew;
+        }
+
     }
 }
